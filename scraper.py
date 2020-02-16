0c5a29f02507256883efcae99713f8aff4cc9a09
Ziyu Chen <zc839@nyu.edu>
2020-02-16 23:50:31 +0800

    Finished the scraper. Collected some sample data from Zhihu.

--- scraper.py
+++ scraper.py
@@ -1,30 +1,45 @@
 import requests
 import json
-from bs4 import BeautifulSoup
+import pandas as pd
+
+# Replace it with the path of the folder
+path = '/Users/ZiyuChen/assignment/'
 
 def index():
+    
     headers={
         'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.79 Safari/537.36'
     }
+
+    # Get data from https://www.zhihu.com/special/19681091/trends#map
     response = requests.get('https://www.zhihu.com/special/19681091/trends#map', headers=headers)
     response.encoding = 'utf-8'
     text = response.text
+
     # Get timestamp
     timestamp = get_string('"deadline":"', text)
-    print(timestamp)
 
     # Get oversea data
-    oversea = get_array('"overseasList":', text)
-    print(oversea)
+    foreign_countries = get_array('"overseasList":', text)
+    foreign_countries_df = pd.DataFrame(foreign_countries)[['name', 'conNum', 'cureNum', 'deathNum', 'susNum']]
+    foreign_countries_df.columns = ['国家', '累计确诊人数', '治愈人数', '死亡人数', '疑似病例']
+    foreign_countries_df.to_csv(path + '疫情实时动态爬虫/外国数据（国别）/' + timestamp + '.csv')
 
     # Get domestic data
-    domestic = get_array('"domesticList":', text)
-    print(domestic)
-    
+    provinces = get_array('"domesticList":', text)
+
+    # Unravel domestic data
+    cities = unravel(provinces)
+    cities_df = pd.DataFrame(cities)[['province_name', 'name', 'conNum', 'cureNum', 'deathNum', 'susNum']]
+    cities_df.columns = ['省', '市', '累计确诊人数', '治愈人数', '死亡人数', '疑似病例']
+    cities_df.to_csv(path + '疫情实时动态爬虫/中国数据（市别）/' + timestamp + '.csv')
+
+
 def get_string(signal, text):
     start = text.index(signal) + len(signal)
     end = text.index('"', start)
     return text[start:end]
+
 
 def get_array(signal, text):
     start = text.index(signal) + len(signal)
@@ -37,6 +52,16 @@ def get_array(signal, text):
         if count == 0:
             return json.loads(text[start:i+1])
 
+
+def unravel(provinces):
+    store = []
+    for province in provinces:
+        province_name = province['name']
+        cities = province['cities']
+        for city in cities:
+            city['province_name'] = province_name
+        store += cities
+    return store
 
 
 if __name__ == '__main__':
